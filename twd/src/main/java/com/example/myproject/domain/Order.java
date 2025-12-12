8b1d8f2ecd02598fae5b65dc2748ffc8c897d607
amy-le <nthuyngoc2002@gmail.com>
2025-12-12 15:40:43 +1100

    Fix lỗi tạo table Order

---
+++ twd/src/main/java/com/example/myproject/domain/Order.java
@@ -0,0 +1,36 @@
+package com.example.myproject.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double totalPrice;
+    private User user;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", totalPrice=" + totalPrice + ", user=" + user + "]";
+    }
+    
+}
